31bd1a68c035b88e019cf1213cb1810b6688c4fc
Berta Devant <bertadeva@gmail.com>
2017-07-25 11:03:48 +0200

    Add a MainNavigator

---
+++ Calculator/ChooseCalculatorAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  ChooseCalculatorAdapter.swift
+//  Calculator
+//
+//  Created by Berta Devant on 21/07/2017.
+//  Copyright © 2017 Berta Devant. All rights reserved.
+//
+
+import Foundation
---
+++ Calculator/MainNavigator.swift
@@ -0,0 +1,9 @@
+//
+//  MainNavigator.swift
+//  Calculator
+//
+//  Created by Berta Devant on 24/07/2017.
+//  Copyright © 2017 Berta Devant. All rights reserved.
+//
+
+import Foundation
